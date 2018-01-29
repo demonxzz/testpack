2637574eb550a58db5e17941dcd484e6132d4042
CoderJam <422361622@qq.com>
2018-01-29 11:46:24 +0800

    debug

---
+++ src/Admin/Admin.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Test\Admin;
+
+class Admin
+{
+    public function printRunning()
+    {
+        echo 'running';
+    }
+}
---
+++ src/Admin/AdminServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Test\Admin;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        $this->app->singleton('admin', function () {
+            return new Admin();
+        });
+    }
+}
